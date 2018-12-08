6080a06f3ff9eb66c1f0950e97033d2b34d70c76
cmarsh1 <cmarsh1@swarthmore.edu>
2018-12-08 13:58:24 -0500

    degrade pencil point when characters written

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -7,18 +7,38 @@ public class Pencil {
 
     }
 
+
     public Pencil(Paper paper) {
         this.paper = paper;
     }
+
 
     public Pencil(Paper paper, int durability) {
         this.paper = paper;
         this.durability = durability;
     }
 
+
     public void write(String text) {
-        if(paper != null) {
-            paper.append(text);
+        if (paper == null || text == null) {
+            return;
         }
+
+        paper.append(text);
+
+        for (int i = 0; i < text.length(); i++) {
+            char currChar = text.charAt(i);
+            if (currChar >= 65 && currChar <= 90){
+                durability -= 2;
+            } else if (currChar > 32 && currChar < 127){
+                durability--;
+            }
+        }
+
+    }
+
+
+    public int getDurability () {
+        return durability;
     }
 }
--- src/test/java/PencilTest.java
+++ src/test/java/PencilTest.java
@@ -5,49 +5,68 @@ import static org.junit.Assert.assertEquals;
 
 public class PencilTest {
 
-    Pencil pencil;
+    Pencil defaultPencil;
+    Pencil softPencil;
+    Pencil durablePencil;
     Paper paper;
+
 
     @Before
     public void setUp() {
         paper = new Paper();
-        pencil = new Pencil(paper);
+        defaultPencil = new Pencil(paper, 1000);
+        softPencil = new Pencil(paper, 10);
+        durablePencil = new Pencil(paper, 200);
     }
 
     
     @Test
     public void pencilRecordsStringWhenWritten() {
-        pencil.write("hello");
+        defaultPencil.write("hello");
         assertEquals("hello", paper.read());
     }
 
     
     @Test
     public void pencilRecordsAnyStringWhenWritten() {
-        pencil.write("The quick brown fox jumped over the lazy dog.");
+        defaultPencil.write("The quick brown fox jumped over the lazy dog.");
         assertEquals("The quick brown fox jumped over the lazy dog.", paper.read());
     }
 
     
     @Test
     public void pencilConcatenatesTextToRecord() {
-        pencil.write("Hello. ");
-        pencil.write("My name is Inigo Montoya. ");
-        pencil.write("You killed my father. ");
-        pencil.write("Prepare to die.");
+        defaultPencil.write("Hello. ");
+        defaultPencil.write("My name is Inigo Montoya. ");
+        defaultPencil.write("You killed my father. ");
+        defaultPencil.write("Prepare to die.");
         assertEquals("Hello. My name is Inigo Montoya. You killed my father. Prepare to die.", paper.read());
     }
 
 
     @Test
     public void pencilCanWriteNothing() {
-        pencil.write("Winter is coming.");
-        pencil.write(null);
+        defaultPencil.write("Winter is coming.");
+        defaultPencil.write(null);
         assertEquals("Winter is coming.", paper.read());
     }
 
+
     @Test
     public void canProvidePointDurabilityToPencilOnInstantiation() {
-        Pencil durablePencil = new Pencil(paper, 200);
+        Pencil pencilWithDurability = new Pencil(paper, 200);
     }
+
+    @Test
+    public void pencilPointDegradesInValueWhenCharactersAreWritten() {
+        durablePencil.write("Hello, world!");
+        assertEquals(187, durablePencil.getDurability());
+    }
+
+    //    @Test
+//    public void pencilWritesSpaceWhenPointIsDull() {
+//        Pencil softLeadPencil = new Pencil(paper, 10);
+//        softLeadPencil.write("Hello, world!");
+//        assertEquals("Hello, wor   ", paper.read());
+//    }
 }
